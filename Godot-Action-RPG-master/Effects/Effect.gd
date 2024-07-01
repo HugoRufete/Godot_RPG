01c34c9bb799d36dd1c4e079a36da2f412003ba1
HugoRufete <hugorufete@gmail.com>
2024-07-01 20:32:57 +0200

    Proyecto con funcionalidad básica

---
+++ Godot-Action-RPG-master/Effects/Effect.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+
+func _ready():
+	connect("animation_finished", Callable(self, "_on_animation_finished"))
+	play("Animate")
+
+
+func _on_animation_finished():
+	queue_free()
